673e52f5976d15c87224eb376d762ee18595270f
Jamie Moore <jamiemoore86@gmail.com>
2022-11-06 17:56:53 -0500

    Added code to color in squares based on color

--- designs.js
+++ designs.js
@@ -1,38 +1,42 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+//Variable setup
 const sizePickerForm = document.getElementById("sizePicker");
 const pixelCanvasTable = document.getElementById("pixelCanvas")
 
+let chosenColor = document.getElementById("colorPicker");
+
+//Look for submit button click
 sizePickerForm.addEventListener("submit", makeGrid, false);
 
+//Create the grid and set up event listeners
 function makeGrid(submitButton) {
     submitButton.preventDefault();
-    // Your code goes here!
 
-    //Clear Grid
+    //Clear Grid before creating new one
     pixelCanvasTable.innerHTML = "";
 
-    console.log("We reached the grid function.");
-
-    //Add tr and td tags to table to create grid.
-
-    //Get values from form inputs
+    //Get values from form input form
     const height = document.getElementById("inputHeight").value;
     const width = document.getElementById("inputWidth").value;
 
+    //Loop based on chosen height
     for(var i = 0; i <= height; i++)
     {
         let trElement = document.createElement("tr");
 
+        //Loop based on chosen width
         for(var j = 0; j <= width; j++)
         {
             let tdElement = document.createElement("td");
+            //Add event listener for each td element and call changeColor
+            tdElement.addEventListener("click", changeColor, false);
             trElement.appendChild(tdElement);
         }
 
         pixelCanvasTable.appendChild(trElement);
     }
+}
 
+//Apply the color to the clicked cell
+function changeColor(clickedCell) {
+    clickedCell.target.style.backgroundColor = chosenColor.value;
 }
